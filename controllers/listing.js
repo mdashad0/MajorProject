0baaf1f5334efff9b7071faf9a7eb99d1c5cf9f2
Md Ashad <asadanwarr0@gmail.com>
2025-11-22 21:40:38 +0530

    Remove unnecessary blank line in listing.js

--- controllers/listing.js
+++ controllers/listing.js
@@ -7,7 +7,6 @@ module.exports.index=async (req, res) => {
 
     const allListings = await Listing.find({});
     res.render("listings/index.ejs", { allListings });
-
 };
 
 module.exports.renderNewForm=(req, res) => {
