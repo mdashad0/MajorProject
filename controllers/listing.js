5b78d29bd72a01c857510a84f6ff7bf68213eaca
Md Ashad <asadanwarr0@gmail.com>
2025-11-22 21:43:16 +0530

    Update listing.js

--- controllers/listing.js
+++ controllers/listing.js
@@ -13,7 +13,6 @@ module.exports.renderNewForm=(req, res) => {
 
     res.render("listings/new.ejs");
 };
-
 module.exports.showListings=async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id).populate({path:"reviews",populate:{path:"author"},}).populate("owner");
